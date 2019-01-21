4995b11268062ac1db180f9c0de5cef86669a0ff
Meng Guang <mengguang@newtonproject.org>
2019-01-21 13:54:17 +0800

    i2s dac works.

---
+++ Src/main.c
@@ -0,0 +1,284 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * Copyright (c) 2019 STMicroelectronics International N.V. 
+  * All rights reserved.
+  *
+  * Redistribution and use in source and binary forms, with or without 
+  * modification, are permitted, provided that the following conditions are met:
+  *
+  * 1. Redistribution of source code must retain the above copyright notice, 
+  *    this list of conditions and the following disclaimer.
+  * 2. Redistributions in binary form must reproduce the above copyright notice,
+  *    this list of conditions and the following disclaimer in the documentation
+  *    and/or other materials provided with the distribution.
+  * 3. Neither the name of STMicroelectronics nor the names of other 
+  *    contributors to this software may be used to endorse or promote products 
+  *    derived from this software without specific written permission.
+  * 4. This software, including modifications and/or derivative works of this 
+  *    software, must execute solely and exclusively on microcontroller or
+  *    microprocessor devices manufactured by or for STMicroelectronics.
+  * 5. Redistribution and use of this software other than as permitted under 
+  *    this license is void and will automatically terminate your rights under 
+  *    this license. 
+  *
+  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS" 
+  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT 
+  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+  * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
+  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT 
+  * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "dma.h"
+#include "i2c.h"
+#include "i2s.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "SEGGER_RTT.h"
+#include "wm8731.h"
+#include "stdbool.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* Audio file size and start offset address are defined here since the audio wave file is
+   stored in Flash memory as a constant table of 16-bit data
+ */
+#define AUDIO_FILE_SIZE               411444        /* Size of audio file */
+#define AUDIO_START_OFFSET_ADDRESS    44            /* Offset relative to audio file header size */
+#define AUDIO_FILE_ADDRESS            0x08010000    /* Audio file address */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+int _write(int file, char *ptr, int len) {
+	(void) file; /* Not used, avoid warning */
+	SEGGER_RTT_Write(0, ptr, len);
+	return len;
+}
+
+__IO uint8_t I2S_TX_CPLT = 0;
+
+void HAL_I2S_TxCpltCallback(I2S_HandleTypeDef *hi2s)
+{
+ /* Prevent unused argument(s) compilation warning */
+ UNUSED(hi2s);
+ I2S_TX_CPLT = 1;
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_USART2_UART_Init();
+  MX_I2S2_Init();
+  MX_I2C1_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* Retrieve Wave Sample rate */
+  WAVE_FormatTypeDef *waveformat =  NULL;
+  waveformat = (WAVE_FormatTypeDef*)AUDIO_FILE_ADDRESS;
+  wm8731_display_wav_info(waveformat);
+  wm8731_dac_init();
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+	  HAL_GPIO_TogglePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin);
+	  printf("Begin to play music!\n");
+	  uint32_t begin = AUDIO_FILE_ADDRESS + AUDIO_START_OFFSET_ADDRESS;
+	  uint32_t current = 0;
+	  uint32_t total = AUDIO_FILE_SIZE - AUDIO_START_OFFSET_ADDRESS;
+	  uint32_t block_size = 16384;
+	  while(current < total) {
+		  if(total - current < block_size) {
+			  block_size = total - current;
+		  }
+		  printf("current: %lu\n",current);
+		  HAL_I2S_Transmit_DMA(&hi2s2, (uint16_t *)(begin + current), (block_size / 2));
+		  while(I2S_TX_CPLT == 0);
+		  I2S_TX_CPLT = 0;
+		  current += block_size;
+	  }
+
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
+
+  /**Configure the main internal regulator output voltage 
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /**Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 180;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 8;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /**Activate the Over-Drive mode 
+  */
+  if (HAL_PWREx_EnableOverDrive() != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /**Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_I2S_APB1;
+  PeriphClkInitStruct.PLLI2S.PLLI2SN = 50;
+  PeriphClkInitStruct.PLLI2S.PLLI2SP = RCC_PLLI2SP_DIV2;
+  PeriphClkInitStruct.PLLI2S.PLLI2SM = 8;
+  PeriphClkInitStruct.PLLI2S.PLLI2SR = 2;
+  PeriphClkInitStruct.PLLI2S.PLLI2SQ = 2;
+  PeriphClkInitStruct.PLLI2SDivQ = 1;
+  PeriphClkInitStruct.I2sApb1ClockSelection = RCC_I2SAPB1CLKSOURCE_PLLI2S;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{ 
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ Src/wm8731.c
@@ -0,0 +1,78 @@
+/*
+ * wm8731.c
+ *
+ *  Created on: 2019Äê1ÔÂ21ÈÕ
+ *      Author: mengguang
+ */
+
+
+#include "wm8731.h"
+
+void wm8731_display_wav_info(WAVE_FormatTypeDef * format)
+{
+  printf("Sampling frequency : %lu Hz\n", format->SampleRate);
+  if (format->NbrChannels == 2)
+  {
+    printf("Format : %d bits stereo\n", format->BitPerSample);
+  }
+  else if (format->NbrChannels == 1)
+  {
+    printf("Format : %d bits mono\n", format->BitPerSample);
+  }
+}
+
+
+//------------------------------------------------------------------------------
+/// Write data to WM8731 Register.
+/// \param regAddr       register address to read.
+/// \param data    data to write
+//------------------------------------------------------------------------------
+bool wm8731_reg_write(uint8_t regAddr, uint16_t data)
+{
+    uint8_t tmpData[2];
+    uint16_t tmp;
+    tmp = ((regAddr & 0x7f) << 9) | (data & 0x1ff);
+
+    tmpData[0] = (tmp & 0xff00) >> 8;
+    tmpData[1] = tmp & 0xff;
+    HAL_StatusTypeDef st = HAL_I2C_Master_Transmit(&hi2c1, WM8731_SLAVE_ADDRESS << 1, tmpData, 2, 1000);
+    if(st != HAL_OK) {
+    	printf("WM8731_Write HAL_I2C_Master_Transmit error: %d\n",st);
+    	return false;
+    }
+    return true;
+}
+
+//------------------------------------------------------------------------------
+/// Init WM8731 to DAC mode.
+//------------------------------------------------------------------------------
+bool wm8731_dac_init()
+{
+    // reset
+    wm8731_reg_write(WM8731_REG_RESET, 0);
+
+    // analogue audio path control
+    wm8731_reg_write(WM8731_REG_ANALOGUE_PATH_CTRL, 0x12);
+
+    // digital audio path control
+    wm8731_reg_write(WM8731_REG_DIGITAL_PATH_CTRL, 0x00);
+
+    // power down control
+    wm8731_reg_write(WM8731_REG_PWDOWN_CTRL, 0x7);
+
+    // digital auduo interface format
+    wm8731_reg_write(WM8731_REG_DA_INTERFACE_FORMAT,0b00010010);
+
+    // sampling control
+    wm8731_reg_write(WM8731_REG_SAMPLECTRL,0b00001100);
+
+    // volume
+    wm8731_reg_write(WM8731_REG_LEFT_HPOUT, 0b01100001);
+    wm8731_reg_write(WM8731_REG_RIGHT_HPOUT,0b01100001);
+
+    // Active control
+    wm8731_reg_write(WM8731_REG_ACTIVE_CTRL, 0x01);
+
+    return true;
+}
+
